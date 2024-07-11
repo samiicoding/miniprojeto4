261075cdd543b2c8bcb68178ebf6888d0ef8ee76
sanbergsamira34@gmail.com <sanbergsamira34@gmail.com>
2024-07-11 10:53:36 -0300

    criando pastas e iniciando códigos

---
+++ src/models/people.model.js
@@ -0,0 +1,9 @@
+export class People{
+    constructor(id,name,biography, category,photo_url){
+        this.id = id;
+        this.name = name;
+        this.biography = biography;
+        this.category = category;
+        this.photo_url = photo_url;
+    }
+}
---
+++ src/server.js
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.listen (port, ()=>{
+    console.log ('exemple app listening on port ${port}'
+    )
+})
